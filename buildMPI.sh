ee2af71b89b97bc32ca384a9f6870aeb60101e00
Anthony <onehalfatsquared@github.com>
2019-05-03 16:05:16 -0400

    cmake files for mpi compiler

--- buildMPI.sh
+++ buildMPI.sh
@@ -2,5 +2,5 @@
 cmake .. \
 	-DCMAKE_CXX_COMPILER="mpic++"\
 	-DCMAKE_C_COMPILER="mpicc"\
-	-DCMAKE_CXX_FLAGS=" -std=c++11 -lfftw3 -lpthread -lm -g"\
-	-DCMAKE_C_FLAGS=" -lfftw3 -lpthread -lm "
+	-DCMAKE_CXX_FLAGS=" -std=c++11 -lfftw3 --lfftw3_mpi -lpthread -lm -g"\
+	-DCMAKE_C_FLAGS=" -lfftw3 --lfftw3_mpi -lpthread -lm "
